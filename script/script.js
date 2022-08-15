d72e17832da15f36937d9306f5411728ba1c04f2
naiiiden <na4ko359@gmail.com>
2022-08-15 08:18:40 +0300

    hover chart price show price from data.json

--- script/script.js
+++ script/script.js
@@ -2,4 +2,11 @@ fetch("script/data.json")
     .then(response => response.json())
     .then(data => {
         console.log(data);
+        document.querySelector(".price--mon").textContent = "$" + data[0].amount;
+        document.querySelector(".price--tue").textContent = "$" + data[1].amount;
+        document.querySelector(".price--wed").textContent = "$" + data[2].amount;
+        document.querySelector(".price--thu").textContent = "$" + data[3].amount;
+        document.querySelector(".price--fri").textContent = "$" + data[4].amount;
+        document.querySelector(".price--sat").textContent = "$" + data[5].amount;
+        document.querySelector(".price--sun").textContent = "$" + data[6].amount;
     });
